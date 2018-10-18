4d2ede2cae0285bd52437adc0d42f50dbe193e72
Miguel Maestre <maestre.95@gmail.com>
2018-10-18 07:01:50 -0400

    modelo estandard

--- app.js
+++ app.js
@@ -8,51 +8,143 @@ let soluciones = [];
 let tablaRes = [];
 let modeloEstandard;
 
+//
 function simplex() {
 	let objetivo = document.getElementById('obj-input').value
 	
-  modeloEstandard = {
-		tipo: document.getElementById('tipo').value,
-		objetivo: [],
-		restricciones: [],
-		varDecision: [],
-		varHolgura: [],
-		varArtificiales: [],
-	}
+	
+	let modelo = estandarizar();
 
-	variablesRea = objetivo.match(/[X]\d{1,}/g)
+	let tablaInicial = [];
 
-	variablesHol.forEach(s => {
-		objetivo += ' + 0'+s;
-		modeloEstandard.varHolgura.push(s)
+	tablaInicial.push(getCoeficients(modelo.objetivo));
+
+	modelo.restricciones.forEach(r => {
+		tablaInicial.push(getCoeficients(r));
 	});
 
-	variablesArt.forEach(r => {
-		objetivo += ' + 0'+r;
-		modeloEstandard.varArtificiales.push(r)
-	})
+	// Crear cabecera de la tabla
+	// con variables en funcion objetivo
+	let variables = modelo.objetivo.match(/[a-zA-Z]\d{1,}/g);
 
-	getCoeficients(objetivo).forEach(el => {
-		(el == 0) ? modeloEstandard.objetivo.push(0) : modeloEstandard.objetivo.push(el*(-1))
-	})
+	let cabecera = document.getElementById('cabecera');
+	cabecera.innerHTML = '<th>VB</th>';
 
-	modeloEstandard.funcionObjetivo = objetivo;
+	variables.forEach(x => {
+		cabecera.innerHTML += '<th>' + x + '</th>';
+	})
+	// Seleccionar variables Basicas
+	// Crear columna de variables basicas
+
+	let tabla = document.getElementById('tbody');
+	tabla.innerHTML = '';
+
+	tablaInicial.forEach(fila => {
+		let filaHtml = '<tr><td>Z</td>';
+		
+		fila.forEach(coe => {
+			filaHtml += '<td>' + coe + '</td>';
+		});
+
+		tabla.innerHTML += filaHtml + '</tr>';
+	});
+
 
 }
 
-function addRestriction(e) {
+// Metodo para agregar una restriccion al arreglo de restricciones
+function agregarRestriccion(e) {
 	let input =  document.getElementById('restriction-input');
 	let res = input.value;
 
-	if (res == '') return
+	if (res == '' || (/(\s=\s)|(\s>=\s)|(\s<=\s)/g).test(res) == false) return
 
-	restricciones.push(splitRestriction(res))
-	tablaRes.push(getCoeficients(restricciones[restricciones.length -1][1].rightHand))
-	soluciones.push(restricciones[restricciones.length -1][1].leftHand)
+	restricciones.push(res);
+	//tablaRes.push(getCoeficients(restricciones[restricciones.length -1][1].rightHand))
+	//soluciones.push(restricciones[restricciones.length -1][1].leftHand)
 	input.value = '';
 
-	//console.log(restricciones)
 	document.getElementById('restriction-list').innerHTML += `<li class="list-group-item"><code>${res}</code></li>`
+}
+
+// Metodo para transformar el modelo de PL a su forma Estandard
+function estandarizar() {
+	let c_restricciones = restricciones.length; // cantidad de restricciones
+	let v_artificiales = 0;	// contador de variables artificiales
+	let v_holgura = 0;	// contdor variables de holgura
+
+
+	let modelo = { // Objeto de Modelo estandard.
+		tipo: document.getElementById('tipo').value,
+		objetivo: document.getElementById('obj-input').value,
+		restricciones: [],
+	};
+
+	// determina catidad de cada tipo de variable
+	restricciones.forEach((res) => {
+		if (/\s=\s/g.test(res)) {
+			v_artificiales += 1;
+			modelo.objetivo += '+0R'+v_artificiales;
+		} else if (/\s<=\s/g.test(res)) {
+			v_holgura += 1;
+			modelo.objetivo += '+0S'+v_holgura;
+		} else if (/\s>=\s/g.test(res)) {
+			v_holgura += 1;
+			v_artificiales += 1;
+			modelo.objetivo += '+0R'+v_artificiales;
+			modelo.objetivo += '+0S'+v_holgura;
+		}
+	});
+
+	let c_art = 0; // contador de variables artificiales agregadas
+	let c_hol = 0; // contador de variables de holgura agregadas
+
+	restricciones.forEach((res) => {
+		let estandard = res;
+		
+		// agrega variables de holgura a la restriccion
+		for (let i = 1; i <= v_holgura; i++) {
+			estandard = estandard.replace(' ', ('+0S'+ i + ' '));
+		}
+
+		// agrega variables artificiales a la restriccion
+		for (let j = 1; j <= v_artificiales; j++) {
+			estandard = estandard.replace(' ', ('+0R'+ j + ' '));
+		}
+
+		// cambia el coeficiente de las variables de exceso y holgura correspondintes a la retriccion
+		if (/\s=\s/g.test(estandard)) {
+			estandard = estandard.replace('0R'+ (c_art+1), '1R'+ (c_art+1) );
+			c_art += 1;
+		} 
+
+		if (/\s<=\s/g.test(estandard)) {
+			estandard = estandard.replace('0S'+ (c_hol+1), '1S'+ (c_hol+1) );
+			c_hol += 1;
+		} 
+
+		if (/\s>=\s/g.test(estandard)) {
+			estandard = estandard.replace('0R'+ (c_art+1), '1R'+ (c_art+1) );
+			c_art += 1;
+			estandard = estandard.replace('+0S'+ (c_hol+1), '-1S'+ (c_hol+1) );
+			c_hol += 1;
+		}
+
+		estandard = estandard.replace(/=|>=|<=/g, '='); // cambia desigualdad por '='
+
+		modelo.restricciones.push(estandard); // agrega la restriccion en forma estandard al modelo
+	})
+
+	/*
+	console.log('restricciones:', c_restricciones),
+	console.log('holgura:', v_holgura);
+	console.log('artificial:', v_artificiales);
+	console.log('\n----- Modelo Estandard ------');
+	console.log(modelo.tipo,'Z =', modelo.objetivo);
+	console.log('Sujeto a:');
+	modelo.restricciones.forEach(r => console.log(r));
+	*/
+	return modelo;
 }
 
 function splitRestriction(res) {
@@ -80,6 +172,7 @@ function splitRestriction(res) {
 	}
 }
 
+// Metodo para extraer los coeficientes de una expresion
 function getCoeficients(exp) {
 	//console.log(exp)
 	let expTree = exp.split(/(\+)|(\-)/g);
@@ -105,7 +198,6 @@ function getCoeficients(exp) {
 	});
 
 	//console.log(coe);
-
 	return coe;
 
 }
@@ -118,7 +210,6 @@ function crearTabla() {
 	let tabla = document.getElementById('tabla');
 
 	let body = tabla.querySelector('tbody');
-
 
 	tablaRes.forEach((el, i) => {
 		let tr = document.createElement('tr');
